331d3bd484cd488877348090526a0c5d2b6bd3ff
ThinkPrivacy <a.hanff@think-privacy.com>
2015-05-10 22:37:29 +0200

    Added Register, Login and Logout + JWT support

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -24,7 +24,28 @@ app.config([
 						return posts.get($stateParams.id);
 					}]
 				}
+			})
+			.state('login', {
+				url: '/login',
+				templateUrl: '/login.html',
+				controller: 'AuthCtrl',
+				onEnter: ['$state', 'auth', function($state, auth){
+					if(auth.isLoggedIn()){
+						$state.go('home');
+					}
+				}]
+			})
+			.state('register', {
+				url: '/register',
+				templateUrl: '/register.html',
+				controller: 'AuthCtrl',
+				onEnter: ['$state', 'auth', function($state, auth){
+					if(auth.isLoggedIn()){
+						$state.go('home');
+					}
+				}]
 			});
+
 		$urlRouterProvider.otherwise('home');
 }]);
 
@@ -72,6 +93,54 @@ app.factory('posts', ['$http', function($http){
 	return o;
 }]);
 
+app.factory('auth', ['$http', '$window', function($http, $window){
+	var auth = {};
+
+	auth.saveToken = function(token){
+		$window.localStorage['flapper-news-token'] = token;
+	};
+
+	auth.getToken = function(){
+		return $window.localStorage['flapper-news-token'];
+	};
+
+	auth.isLoggedIn = function(){
+		var token = auth.getToken();
+		if(token){
+			var payload = JSON.parse($window.atob(token.split('.')[1]));
+			return payload.exp > Date.now() / 1000;
+		} else {
+			return false;
+		}
+	};
+
+	auth.currentUser = function(){
+		if(auth.isLoggedIn()){
+			var token = auth.getToken();
+			var payload = JSON.parse($window.atob(token.split('.')[1]));
+			return payload.username;
+		}
+	};
+
+	auth.register = function(user){
+		return $http.post('/register', user).success(function(data){
+			auth.saveToken(data,token);
+		});
+	};
+
+	auth.logIn = function(user){
+		return $http.post('/login', user).success(function(data){
+			auth.saveToken(data.token);
+		});
+	};
+
+	auth.logOut = function(){
+		$window.localStorage.removeItem('flapper-news-token');
+	};
+
+	return auth;
+}]);
+
 app.controller('MainCtrl', [
 	'$scope',
 	'posts',
@@ -116,4 +185,35 @@ app.controller('PostsCtrl', [
 			posts.upvoteComment(post,comment);
 		};
 		
-	}]);
+}]);
+
+app.controller('AuthCtrl', [
+	'$scope',
+	'$state',
+	'auth',
+	function($scope, $state, auth){
+		$scope.user = {};
+		$scope.register = function(){
+			auth.register($scope.user).error(function(error){
+				$scope.error = error;
+			}).then(function(){
+				state.go('home');
+			});
+		};
+		$scope.logIn = function(){
+			auth.logIn($scope.user).error(function(error){
+				$scope.error = error;
+			}).then(function(){
+				$state.go('home');
+			});
+		};
+}]);
+
+app..controller('NavCtrl', [
+	'$scope',
+	'auth',
+	function($scope, auth){
+		$scope.isLoggedIn = auth.isLoggedIn;
+		$scope.currentUser = auth.currentUser;
+		$scope.logOut = auth.logOut;
+}]);
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var User = mongoose.model('User');
+var jwt = require('express-jwt');
+var auth = jwt({secret: process.env.SECRET, userProperty: 'payload'});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -18,8 +22,9 @@ router.get('/posts', function(req, res, next) {
   });
 });
 
-router.post('/posts', function(req, res, next) {
+router.post('/posts', auth, function(req, res, next) {
   var post = new Post(req.body);
+  post.author = req.payload.username;
 
   post.save(function(err, post){
     if(err){ return next(err); }
@@ -58,7 +63,7 @@ router.get('/posts/:post', function(req, res, next) {
   });
 });
 
-router.put('/posts/:post/upvote', function(req, res, next) {
+router.put('/posts/:post/upvote', auth, function(req, res, next) {
   req.post.upvote(function(err, post){
     if (err) { return next(err); }
 
@@ -66,9 +71,10 @@ router.put('/posts/:post/upvote', function(req, res, next) {
   });
 });
 
-router.post('/posts/:post/comments', function(req, res, next) {
+router.post('/posts/:post/comments', auth, function(req, res, next) {
   var comment = new Comment(req.body);
   comment.post = req.post;
+  comment.author = req.payload.username;
 
   comment.save(function(err, comment){
     if(err){ return next(err); }
@@ -82,11 +88,40 @@ router.post('/posts/:post/comments', function(req, res, next) {
   });
 });
 
-router.put('/posts/:post/comments/:comment/upvote', function (req, res, next) {
+router.put('/posts/:post/comments/:comment/upvote', auth, function (req, res, next) {
     req.comment.upvote(function (err, comment) {
         if (err) { return next(err); }
         res.json(comment);
     });
+});
+
+router.post('/register', function(req, res, next){
+	if(!req.body.username || !req.body.password){
+		return res.status(400).json({message: 'Please fill out all fields'});
+	}
+	var user = new User();
+	user.username = req.body.username;
+	user.setPassword(req.body.password);
+	user.save(function(err){
+		if(err){ return next(err); }
+		return res.json({token: user.generateJWT()})
+	});
+});
+
+router.post('/login', function(req,res,next){
+	if(!req.bodu.username || !req.body.password){
+		resturn res.status(400).json({message: 'Please fill out all fields'});
+	}
+
+	passport.authenticate('local', function(err,user,info){
+		if(err){ return next(err); }
+
+		if(user){
+			return res.json({token: user.generateJWT()});
+		} else {
+			res.status(401).json(info);
+		}
+	})(req, res, next);
 });
 
 module.exports = router;
