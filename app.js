8ab691dc68578751e8d3dcefdcc9c3a1a58954f9
ThinkPrivacy <a.hanff@think-privacy.com>
2015-05-10 21:56:10 +0200

    Added Passport/Passport-local

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/news');
 require('./models/Posts');
 require('./models/Comments');
+require('./models/Users');
+
+require('./config/passport');
+var passport = require('passport');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -26,6 +30,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', routes);
 app.use('/users', users);
